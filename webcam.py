2346334a77c3c993763c070e717f855b969944c5
4lex <alexis.monthoux@bluewin.ch>
2024-11-16 12:44:01 +0100

    update

--- webcam.py
+++ webcam.py
@@ -2,28 +2,26 @@ import cv2
 import socket
 import numpy as np
 
-UDP_IP = "pipoulets.internet-box.ch" 
-UDP_PORT = 1935    
+UDP_IP = "pipoulets.internet-box.ch"
+UDP_PORT = 1935
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 cap = cv2.VideoCapture(0)
 
+print("opening cap")
 if not cap.isOpened():
-    print("Erreur d'ouverture de la webcam")
+    print("could not open cap")
     exit()
 
 while True:
     ret, frame = cap.read()
-    
-    if not ret:
-        print("Erreur lors de la capture de l'image")
-        break
-    
     _, encoded_image = cv2.imencode('.jpg', frame)
+
     encoded_image_bytes = encoded_image.tobytes()
-    sock.sendto(encoded_image_bytes, (UDP_IP, UDP_PORT))
+    print(f"length: {len(encoded_image_bytes)}")
+    # sock.sendto(encoded_image_bytes, (UDP_IP, UDP_PORT))
     cv2.imshow('Webcam Stream', frame)
-    
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
